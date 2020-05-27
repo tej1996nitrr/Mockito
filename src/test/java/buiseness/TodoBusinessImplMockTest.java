9d5e6050db14a3fb313d4516e4fe7e4b3872819d
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-27 20:36:24 +0530

    mock test

---
+++ src/test/java/buiseness/TodoBusinessImplMockTest.java
@@ -0,0 +1,27 @@
+package buiseness;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import data.api.TodoService;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TodoBusinessImplMockTest {
+
+
+
+@Test
+public void usingMockito() {
+    TodoService todoService = mock(TodoService.class);
+    List<String> allTodos = Arrays.asList("Learn Spring MVC",
+            "Learn Spring", "Learn to Dance");
+    when(todoService.retrieveTodos("Username")).thenReturn(allTodos);
+    TodoBusiness todoBusinessImpl = new TodoBusiness(todoService);
+    List<String> todos = todoBusinessImpl
+            .retrieveTodosRelatedToSpring("Username");
+    assertEquals(2, todos.size());
+}
+}
